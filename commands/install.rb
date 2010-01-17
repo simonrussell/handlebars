9a9c706721c14709705a0d76a3d72e873def74bc
Simon Russell <simon@bellyphant.com>
2010-01-18 10:32:36 +1100

    installer for app

---
+++ commands/install.rb
@@ -0,0 +1,18 @@
+require File.join(File.dirname(__FILE__), '../lib/init')
+
+FileUtils.ln_s File.join($HANDLEBARS_BASE, 'handlebars.rb'), File.join($APP_BASE, 'hb')
+FileUtils.mkdir %w(data files recipes tasks templates).map { |d| File.join($APP_BASE, d) }.reject { |d| File.exist?(d) }
+
+unless File.exist?(File.join($APP_BASE, 'servers.yml'))
+  File.open File.join($APP_BASE, 'servers.yml'), 'w' do |f|
+    f.puts "# central database for your servers
+# example:
+# myserver:
+#   server: 1.2.3.4
+#   mirror: http://your.ubuntu.mirror
+#   roles:
+#     - role1
+#     - role2
+"
+  end
+end
