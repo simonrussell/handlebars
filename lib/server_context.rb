e4f15983c729fe9aa754a4615e6ecf8d3de30291
Thomas Sommer <tom@-tombook-.(none)>
2010-01-28 08:59:34 +1100

    Added support to look up a role (and the assigned server) in respect to a specified parent (option: 'with'). This allows us to specify the server we are connected with and we dont run into problems when finding the proper roles.

--- lib/server_context.rb
+++ lib/server_context.rb
@@ -1,15 +1,15 @@
 # Copyright (c) 2010 Tricycle I.T. Pty Ltd
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,35 +17,36 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# 
+#
 # (from http://www.opensource.org/licenses/mit-license.php)
 
 class ServerContext
   include DataTools
-  
+
   attr_reader :hostname
-  
-  def initialize(hostname)
+
+  def initialize(hostname, parent)
     @hostname = hostname
+    @parent = parent
     @recipes = {}
     @tasks = {}
     @included_recipes = []
   end
-  
+
   def ohai(*args)
     @ohai ||= OhaiManager.from_command(self)
-    
+
     if args.empty?
       @ohai
     else
       @ohai.default(*args)
     end
   end
-  
+
   def template(name, fields = {})
     Erberizer.file(app_base_filename('templates', name), fields)
   end
-  
+
   def recipe(name, &block)
     @recipes[name] = block
   end
@@ -53,23 +54,23 @@ class ServerContext
   def cook(*recipe_names)
     recipe_names.flatten.each do |recipe_name|
       recipe_name = recipe_name.to_s
-      
+
       raise "unknown recipe #{recipe_name}" unless @recipes.key?(recipe_name)
-            
+
       unless @included_recipes.include?(recipe_name)
         @included_recipes << recipe_name
 
         log.info "recipe: #{recipe_name}" do
           @recipes[recipe_name].call
-        end        
+        end
       end
     end
   end
-  
+
   def finish_cooking
     @iptables.finish_cooking if @iptables
   end
-  
+
   {
     :package => :PackageManager,
     :gem => :GemManager,
@@ -85,42 +86,42 @@ class ServerContext
   }.each do |name, klass|
     eval "def #{name}(*args)\n@#{name} ||= #{klass}.new(self)\n@#{name}.default(*args) unless args.empty?\n@#{name}\nend"
   end
-  
+
   def log
     @log ||= NiceLogger.new
   end
-  
+
   def group(name)
     log.info name do
       yield
     end
   end
-  
-  def self.setup(hostname, &block)
-    c = ServerContext.new(hostname)
+
+  def self.setup(hostname, parent, &block)
+    c = ServerContext.new(hostname, parent)
     c.send(:preload_recipes_and_tasks)
     c.instance_eval(&block)
     c
   end
-  
+
   def perform(name, *args)
     raise "unknown task #{name}" unless @tasks.key?(name)
-    
+
     code = @tasks[name]
     raise "#{name} needs #{code.arity} arguments, #{args.length} supplied" unless args.length == code.arity
-    
+
     code.call(*args)
   end
-  
+
   def task(name, &block)
     @tasks[name] = block
   end
-  
+
   PASSWORD_PROMPT_REGEX = /\[\*\* password ([^\s]+) \*\*\]/
 
   def password_db(name)
     passwords = read_data_kv('password-db')
-    
+
     if passwords.key?(name)
       passwords[name]
     else
@@ -128,19 +129,19 @@ class ServerContext
       STDOUT.write "[** password #{name} **]"
       STDOUT.flush
       result = STDIN.gets.strip
-      
+
       append_data_kv('password-db', name, result)
-      
+
       result
     end
   end
 
-  def ip_by_role(role)
-    all_ips_by_role(role).first
+  def ip_by_role(role, parent)
+    all_ips_by_role(role, parent).first
   end
-  
-  def all_ips_by_role(role)
-    servers = ServerList.read.by_role(role)
+
+  def all_ips_by_role(role, parent)
+    servers = ServerList.read.by_role(role, parent)
     servers.values.map { |v| v['server'] }
   end
 
@@ -150,7 +151,7 @@ class ServerContext
     # load shared stuff
     preload 'handlebars/tasks', 'task'
     preload 'handlebars/recipes', 'recipe'
-    
+
     # load site specific stuff
     preload 'tasks', 'task'
     preload 'recipes', 'recipe'
@@ -158,12 +159,12 @@ class ServerContext
 
   def preload(dirname, method)
     prefix = File.join($APP_BASE, "#{dirname}/")
-    
+
     Dir.glob("#{prefix}**/*.rb").each do |filename|
       recipe_code = File.read(filename)
       stumpy_filename = filename[prefix.length .. -4]
       instance_eval "#{method} #{stumpy_filename.inspect} do\n#{recipe_code}\nend", "#{dirname}/#{stumpy_filename}", 0
     end
   end
-  
+
 end
--- lib/server_list.rb
+++ lib/server_list.rb
@@ -1,15 +1,15 @@
 # Copyright (c) 2010 Tricycle I.T. Pty Ltd
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,23 +17,23 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# 
+#
 # (from http://www.opensource.org/licenses/mit-license.php)
 
 class ServerList
-  
+
   def initialize(servers)
     @servers = servers
   end
-  
+
   def [](hostname)
     @servers[hostname]
   end
-  
+
   def self.cook(hostname, recipes)
     server_spec = read[hostname]
 
-    ServerContext.setup(hostname) do
+    ServerContext.setup(hostname, server_spec[:with]) do
       log.info "server: #{hostname}" do
         if recipes
           cook recipes
@@ -44,19 +44,19 @@ class ServerList
       end
     end
   end
-  
-  def by_role(name)
+
+  def by_role(name, parent)
     result = {}
-    
+
     @servers.each do |hostname, info|
-      result[hostname] = info if info['roles'].include?(name)
+      result[hostname] = info if (parent.nil? || hostname == parent) && info['roles'].include?(name)
     end
-    
+
     result
   end
-  
+
   def self.read(filename = File.join($APP_BASE, 'servers.yml'))
     new(YAML.load_file(filename))
   end
-  
+
 end
