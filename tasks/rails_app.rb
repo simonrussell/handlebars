f8cc4b566a14e0348cfb7e4a8f7c4b6e5186e4a3
Thomas Sommer <tom@-tombook-.(none)>
2010-01-28 08:22:42 +1100

    Added support for hosts, that shouldn't redirect to the canonical host.

--- tasks/rails_app.rb
+++ tasks/rails_app.rb
@@ -1,15 +1,15 @@
 # Copyright (c) 2010 Tricycle I.T. Pty Ltd
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,42 +17,53 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-# 
+#
 # (from http://www.opensource.org/licenses/mit-license.php)
 
-|app_name, rails_environment, domains|
+|app_name, rails_environment, canonical_host, aliases|
 
 user_name = "#{app_name}_#{rails_environment}"
 db_user_name = user_name[0...16]      # stupid mysql
 secure = local_file?("ssl_cert/#{user_name}/#{user_name}.crt")
 cap_root = File.join('/srv/www', user_name)
 
+alias_list = aliases.keys
+alias_without_redirect_list = aliases.reject { |name, do_redirect| do_redirect }.keys
+
 cook %w(base passenger shared/sphinx_server)
 
 auth.user user_name, :sudoer => true, :home => true, :login_keys => :all
 
 directory cap_root, File.join(cap_root, 'releases'), File.join(cap_root, 'shared'), File.join(cap_root, 'shared/pids'),
-  :mode => '755', 
-  :owner => user_name, 
+  :mode => '755',
+  :owner => user_name,
   :group => user_name
-  
+
 directory File.join(cap_root, 'shared/log'),
-  :mode => '777', 
-  :owner => user_name, 
+  :mode => '777',
+  :owner => user_name,
   :group => user_name
 
 misc.run '[ -e /etc/apache2/mods-enabled/rewrite.load ]' => 'a2enmod rewrite && apache2ctl graceful'
 
-file "/etc/apache2/sites-available/#{user_name}", 
-  :owner => 'root', 
-  :group => 'root', 
+file "/etc/apache2/sites-available/#{user_name}",
+  :owner => 'root',
+  :group => 'root',
   :mode => '644',
-  :content => template('rails/apache-site.erb', :domains => domains, :cap_root => cap_root, :environment => rails_environment),
+  :content =>
+    template(
+      'rails/apache-site.erb',
+      :canonical_host = canonical_host,
+      :aliases => alias_list,
+      :aliases_without_redirect => alias_without_redirect_list,
+      :cap_root => cap_root,
+      :environment => rails_environment
+    ),
   :after => 'apache2ctl graceful'
 
-file File.join(cap_root, 'shared/database.yml'), 
-  :owner => user_name, 
-  :group => user_name, 
+file File.join(cap_root, 'shared/database.yml'),
+  :owner => user_name,
+  :group => user_name,
   :mode => '644',
   :content => template('rails/database.yml.erb', :username => db_user_name, :password => password_db("mysql/user/#{db_user_name}"), :environment => rails_environment, :host => ip_by_role("#{app_name}/#{rails_environment}/db")),
   :after => "[ -e #{File.join(cap_root, 'current/tmp/')} ] && touch #{File.join(cap_root, 'current/tmp/restart.txt')} || true"
@@ -66,19 +77,28 @@ if secure
     :owner => 'www-data',
     :group => 'www-data',
     :mode => '700'
-  
+
   %w(crt key csr).each do |extension|
     file.put "ssl_cert/#{user_name}/#{user_name}.#{extension}", "/etc/apache2/ssl/#{user_name}.#{extension}",
       :owner => 'www-data',
       :group => 'www-data',
       :mode => '400'
   end
-      
-  file "/etc/apache2/sites-available/#{user_name}-ssl", 
-    :owner => 'root', 
-    :group => 'root', 
+
+  file "/etc/apache2/sites-available/#{user_name}-ssl",
+    :owner => 'root',
+    :group => 'root',
     :mode => '644',
-    :content => template('rails/apache-site.erb', :domains => domains, :cap_root => cap_root, :environment => rails_environment, :certificate => user_name),
+    :content =>
+      template(
+        'rails/apache-site.erb',
+        :canonical_host = canonical_host,
+        :aliases => alias_list,
+        :aliases_without_redirect => alias_without_redirect_list,
+        :cap_root => cap_root,
+        :environment => rails_environment,
+        :certificate => user_name
+      ),
     :after => 'apache2ctl graceful'
 
   misc.run "[ -e /etc/apache2/sites-enabled/#{user_name}-ssl ]" => "a2ensite #{user_name}-ssl && apache2ctl graceful"
